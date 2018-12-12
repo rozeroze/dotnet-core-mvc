583e678598b6252cb9890e9c076ef210bde52710
rozeroze <rosettastone1886@gmail.com>
2018-12-12 19:21:47 +0900

    host setting

--- src/Training/Program.cs
+++ src/Training/Program.cs
@@ -19,6 +19,8 @@ namespace Training
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:8000");
     }
 }
