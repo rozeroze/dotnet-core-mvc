e2a52cc5c7c386f6d9d36f9ca1a24d7bde72c4ad
rozeroze <rosettastone1886@gmail.com>
2018-12-21 19:56:11 +0900

    dotnet-packageのコンフリクトが発生するのを修正

--- src/init.sh
+++ src/init.sh
@@ -1,5 +1,5 @@
 cd ~/src/Training
-dotnet add package Microsoft.EntityFrameworkCore.Sqlite
-dotnet tool install -g dotnet-aspnet-codegenerator
-dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 2.1.0
+dotnet tool install -g dotnet-aspnet-codegenerator --version 2.1.5
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version 2.1.5
 dotnet restore
